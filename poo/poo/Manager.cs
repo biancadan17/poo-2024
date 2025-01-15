6c3928590cf1d3f659563386bae4fabe638c0365
Bianca <bianca.dan@student.upt.ro>
2025-01-15 22:04:09 +0200

    cerere aprobabre

--- poo/poo/Manager.cs
+++ poo/poo/Manager.cs
@@ -2,5 +2,20 @@ namespace poo;
 
 public class Manager
 {
-        
+    public void AprobaCererePreAprobare(GestionareCereriPreAprobare gestionare)
+    {
+        Console.Write("Introduceți ID-ul cererii de aprobat: ");
+        int id = int.Parse(Console.ReadLine());
+        var cerere = gestionare.cereri.Find(c => c.Id == id);
+
+        if (cerere != null)
+        {
+            cerere.Aprobat = true;
+            Console.WriteLine("Cerere aprobată cu succes!");
+        }
+        else
+        {
+            Console.WriteLine("Cerere inexistentă.");
+        }
     }
+}
