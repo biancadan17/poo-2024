73664020e8a0ebb1ed193781a7f28e37695540d1
Bianca <bianca.dan@student.upt.ro>
2025-01-15 22:00:54 +0200

    doc

--- poo/poo/DocumentJustificativ.cs
+++ poo/poo/DocumentJustificativ.cs
@@ -2,5 +2,7 @@ namespace poo;
 
 public class DocumentJustificativ
 { 
-        
+    public TipDocument Tip { get; set; }
+    public decimal Suma { get; set; }
+    public string Detalii { get; set; }
 }
